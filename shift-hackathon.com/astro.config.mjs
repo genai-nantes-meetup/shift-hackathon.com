8839fa3882cf93c325b32ce4664bee6ffcd25a8a
Samuel Berthe <dev@samuel-berthe.fr>
2026-05-31 10:20:25 +0200

    add @astrojs/react integration

--- shift-hackathon.com/astro.config.mjs
+++ shift-hackathon.com/astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 export default defineConfig({
   build: {
     format: 'file',
   },
+
   redirects: {
     '/concept-2026': { status: 301, destination: '/concept' },
     '/agenda-2026': { status: 301, destination: '/agenda' },
     '/intervenants-2026': { status: 301, destination: '/intervenants' },
   },
+
+  integrations: [react()],
 });
